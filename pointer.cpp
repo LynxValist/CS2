e67c792392145523af7482d8e8224df631e51a62
Gerald Kyro Villanueva Tungol <78996700+LynxValist@users.noreply.github.com>
2023-01-25 00:39:21 +0000

    pointer

--- pointer.cpp
+++ pointer.cpp
@@ -2,7 +2,47 @@
 
 using namespace std;
 
+void rotate(int *po, int size);
+
+int main(){
+    //int elements[4] = {1,2,3,4};
+    int size = 4;
+    int elements[4] = {1,2,3,4};
+    int *ptr = elements;
+    return 0;
+    rotate(ptr, size);
+    for(int x = 0; x<size; x++){
+        cout<<*(ptr+x)<<endl;
+    }
+}
+
+void rotate(int *po, int size){
+    int temp = *(po+size); 
+    for(int i = 1; i < size; i++){
+        *(po+i) = *(po + i-1);
+    }
+    *po = temp;
+}
+
+/*
+void swap(int *a, int *b);
 int main(void){
+    int num1 = 2, num2 = -3;
+    swap(&num1, &num2);
+}
+void swap(int *a, int*b){
+    int temp = *a; 
+    *a = *b;
+    *b = temp;
+    
+}*/
+ 
+              //int * const ptr = _____
+              //pointer does not change.
+              //const int * const pointer = ____
+              //ultimate constant
+
+
     /*
     int x = 20; 
     int *ptr = nullptr;
@@ -12,7 +52,9 @@ int main(void){
     cout << "The address of x is " << ptr << endl;
     
     cout << "wolf "<< *ptr;
-    return 0;*/
+    return 0;*/                                          //dereferencing and addresses
+
+    /*
     int x = 3;
     x++;
     
@@ -26,4 +68,15 @@ int main(void){
         fooptr++;                           //fooptr before cout is an error;
     }
     cout<<endl;
+    */
+  /*
+   float level;
+   int fptr = &level;
+   */                       //issue 1}
+
+
+/*
+void doubleVal(int val){
+    *val *=2;
 }
+*/                                 //issue 2
